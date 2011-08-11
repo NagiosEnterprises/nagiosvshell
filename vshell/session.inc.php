7492766c44d0854f1d76868342e92624e5c1786f
mguthrie88 <mguthrie88@users.sourceforge.net>
2011-08-11 22:00:01 +0000

    adding translations and config options for gettext support

--- vshell/session.inc.php
+++ vshell/session.inc.php
@@ -4,16 +4,16 @@
 function init_vshell()
 {
 	
-	
-	$loc = setlocale(LC_ALL, LANG, "en_GB.utf8");
+	//gettext support 
+	$loc = setlocale(LC_ALL, LANG, LANG.'utf-8', LANG.'utf8', "en_GB.utf8");
 	if (!isset($loc)) {
 	 echo gettext("Error in setting the correct locale, please report this error with the associated output of  'locale -a' to mguthrie@nagios.com")."<br>";
 	}
 	putenv("LC_ALL=".LANG);
 	putenv("LANG=".LANG);
-	bindtextdomain(BASEURL, '/usr/lib/locale');
+	bindtextdomain(LANG, 'locale/');
 	bind_textdomain_codeset(BASEURL, 'UTF-8');
-	textdomain(BASEURL); 
+	textdomain(LANG); 
 
 
 }
