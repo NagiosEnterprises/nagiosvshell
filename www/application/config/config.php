6216cefec4cd9c74a151a2070e22aff68ff6219a
Chris Laskey <contact@chrislaskey.com>
2014-01-07 14:35:53 -0500

    Updated CI config values

--- www/application/config/config.php
+++ www/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= BASEURL;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -180,7 +180,7 @@ $config['directory_trigger']	= 'd'; // experimental not currently in use
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = LOG_LEVEL; # See index.php
 
 /*
 |--------------------------------------------------------------------------
