8fa818ab092560efd3dd8a0c74cc27b75b787449
Chris Laskey <contact@chrislaskey.com>
2014-01-08 16:25:22 -0500

    Updated formatting

--- www/application/helpers/display_functions_helper.php
+++ www/application/helpers/display_functions_helper.php
@@ -58,7 +58,7 @@
  */
 function build_nav_links() {
 	//build page links based on user's permission level
-	$ci = & get_instance();
+	$ci = &get_instance();
 
 	//generate links based on permissions
 	$base = '/'.BASEURL;
@@ -68,16 +68,16 @@ function build_nav_links() {
 	//NAV LINKS	 are added to a floated <ul>
 	$navlinks .= '<ul class="nav">';
 
- 	//default tactical overview link
+	//default tactical overview link
 	$navlinks .= '<li class="nav"><a href="index.php" class="nav" rel="internal">'.gettext('Tactical Overview').'</a></li>';
 
- 	//hosts
+	//hosts
 	$navlinks .= "<li class='nav'><a href='".$base."/hosts' class='nav' rel='internal'>".gettext('Hosts')."</a></li>";
 
- 	//services
+	//services
 	$navlinks .= "<li class='nav'><a href='".$base."/services' class='nav' rel='internal'>".gettext('Services')."</a></li>";
 
- 	//hostgroups
+	//hostgroups
 	$navlinks .= "<li class='nav'><a href='".$base."/hostgroups' class='nav' rel='internal'>".gettext('Hostgroups')."</a></li>";
 
 	//servicegroups
@@ -92,28 +92,14 @@ function build_nav_links() {
 		$navlinks .= '<div onmouseover="showDropdown("confDrop")" onmouseout="hideDropdown("confDrop")" id="confDrop">';
 		$navlinks .= '<ul>';
 
- 		//hosts
 		$navlinks .= '<li><a class="nav" href="'.$base.'type=object&objtype_filter=hosts_objs">'.gettext('Hosts').'</a></li>';
-
- 		//services
 		$navlinks .= '<li><a class="nav" href="'.$base.'type=object&objtype_filter=services_objs">'.gettext('Services').'</a></li>';
-
- 		//hostgroups
 		$navlinks .= '<li><a class="nav" href="'.$base.'type=object&objtype_filter=hostgroups_objs">'.gettext('Hostgroups').'</a></li>';
-
- 		//servicegroups
 		$navlinks .= '<li><a class="nav" href="'.$base.'type=object&objtype_filter=servicegroups_objs">'.gettext('Servicegroups').'</a></li>';
-
- 		//timeperiods
 		$navlinks .= '<li><a class="nav" href="'.$base.'type=object&objtype_filter=timeperiods">'.gettext('Timeperiods').'</a></li>';
-
- 		//contacts
 		$navlinks .= '<li><a class="nav" href="'.$base.'type=object&objtype_filter=contacts">'.gettext('Contacts').'</a></li>';
-
- 		//contactgroups
 		$navlinks .= '<li><a class="nav" href="'.$base.'type=object&objtype_filter=contactgroups">'.gettext('Contactgroups').'</a></li>';
 
-		//command view
 		if ($ci->nagios_user->is_admin()) {
 			$navlinks .= "<li><a href='".$base."type=object&objtype_filter=commands' class='nav'>".gettext('Commands')."</a></li>\n";
 		}
@@ -146,7 +132,7 @@ function build_nav_links() {
 	$navlinks .= '<a class="nav" onmouseover="showDropdown("reportDrop")" onmouseout="hideDropdown("reportDrop")" href="javascript:void(0)">'.gettext('Reports').'</a>';
 	$navlinks .= '<div onmouseover="showDropdown("reportDrop")" onmouseout="hideDropdown("reportDrop")" id="reportDrop">';
 	$navlinks .= '<ul>';
-	
+
 	$navlinks .= '<li><a class="nav" target="_blank" href="'.CORECGI.'avail.cgi">'.gettext('Availability').'</a></li>\n';
 	$navlinks .= '<li><a class="nav" target="_blank" href="'.CORECGI.'trends.cgi">'.gettext('Trends').'</a></li>\n';
 	$navlinks .= '<li><a class="nav" target="_blank" href="'.CORECGI.'history.cgi?host=all">'.gettext('Alert History').'</a></li>\n';
@@ -184,60 +170,74 @@ function build_nav_links() {
  *
  * USAGE: $tr_class = get_color_code($host)
  */
-function get_color_code($array,$int=false)
-{
-
-	if($int)
+function get_color_code($array, $int=false) {
+	if ($int) {
 		$state = strtolower(return_host_state($array['current_state']));
-	else
+	} else {
 		$state = strtolower($array['current_state']);
-	$vals = array('ok', 'up', 'down', 'warning', 'unknown', 'critical', 'pending');
-	$str = (in_array($state, $vals) ? $state : 'unknown');
+	}
+
+	$vals = array(
+		'ok',
+		'up',
+		'down',
+		'warning',
+		'unknown',
+		'critical',
+		'pending'
+	);
+
+	$str = in_array($state, $vals) ? $state : 'unknown';
+
 	return $str;
 }
 
 /* get_host_status_color($hostname)
  * based on the hostname provided return the color code from get_color_code()
  */
-function get_host_status_color($hostname)
-{
-	$ci = & get_instance();
+function get_host_status_color($hostname) {
+	$ci = &get_instance();
 	$host = $ci->nagios_data->get_details_by('host',$hostname);
 
 	$color = '';
-	if(isset($host))
+	if (isset($host)) {
 		$color = get_color_code($host,true);
-
+	}
 
 	return $color;
 }
 
-
-
-//////////////////////////////////////////////////////
-//expecting a host name
-//used on host details page
-function get_host_comments($host)
-{
-	$ci = & get_instance();
+/* expecting a host name
+ * used on host details page
+ */
+function get_host_comments($host) {
+	$ci = &get_instance();
 
 	$hostcomments = $ci->nagios_data->getProperty('hostcomments');
 	$comments = "";
 
+	foreach ($hostcomments as $comment) {
+		if ($comment['host_name'] == $host) {
+			$entrytime = date('M d H:i\:s\s', $comment['entry_time']);
 
-	foreach($hostcomments as $comment)
-	{
-	   if($comment['host_name'] == $host)
-	   {
-   		$entrytime = date('M d H:i\:s\s', $comment['entry_time']);
-   		$comments .= "<tr><td>".$comment['author']."</td><td>$entrytime</td><td>".$comment['comment_data']."</td><td>
-   					<a href='".CORECMD."cmd_typ=2&com_id=".$comment['comment_id']."' title='".gettext('Delete Comment')."'>
-   					<img class='iconLink' src='views/images/delete.png' alt='Delete' width='15' height='15' /></a></td></tr>\n";
+			$comments .= '<tr>';
+			$comments .= '<td>'.$comment['author'].'</td>';
+			$comments .= '<td>'.$entrytime.'</td>';
+			$comments .= '<td>'.$comment['comment_data'].'</td>';
+			$comments .= '<td>';
+			$comments .= '<a href="'.CORECMD.'cmd_typ=2&com_id='.$comment["comment_id"].'" title="'.gettext("Delete Comment").'">';
+			$comments .= '<img class="iconLink" src="views/images/delete.png" alt="Delete" width="15" height="15" />';
+			$comments .= '</a>';
+			$comments .= '</td>';
+			$comments .= '</tr>';
 		}
-	}//end foreach
+	}
 
-	if($comments == '')
-		$comments .= "<tr><td colspan='4'>".gettext('There are no comments associated with this host')."</td></tr>";
+	if ($comments == '') {
+		$comments .= '<tr>';
+		$comments .= '<td colspan="4">'.gettext('There are no comments associated with this host').'</td>';
+		$comments .= '</tr>';
+	}
 
 	return $comments;
 }
@@ -246,38 +246,40 @@ function get_host_comments($host)
  * used on host details page
  * @TODO replace CORECMD link with a call to core_command_link(...)
  */
-function get_service_comments($host, $service)
-{
-   global $NagiosData;
+function get_service_comments($host, $service) {
+	$ci = &get_instance();
 	$comments = '';
-	$servicecomments = $NagiosData->getProperty('servicecomments');
+	$servicecomments = $ci->nagios_data->getProperty('servicecomments');
 
-	foreach($servicecomments as $comment)
-	{
+	foreach ($servicecomments as $comment) {
 		if ($comment['host_name'] == $host && $comment['service_description'] == $service) {
 			$entrytime = date('M d H:i\:s\s', $comment['entry_time']);
-			$comments .= "<tr><td>".$comment['author']."</td><td>$entrytime</td><td>".$comment['comment_data']."</td><td>
-						<a href='".CORECMD."cmd_typ=4&com_id=".$comment['comment_id']."' title='".gettext('Delete Comment')."'>
-						<img class='iconLink' src='views/images/delete.png' alt='Delete' width='15' height='15' /></a></td></tr>\n";
+			$comments .= '<tr>';
+			$comments .= '<td>'.$comment['author'].'</td>';
+			$comments .= '<td>'.$entrytime.'</td>';
+			$comments .= '<td>'.$comment['comment_data'].'</td>';
+			$comments .= '<td>';
+			$comments .= '<a href="'.CORECMD.'cmd_typ=4&com_id='.$comment['comment_id'].'" title="'.gettext('Delete Comment').'">';
+			$comments .= '<img class="iconLink" src="views/images/delete.png" alt="Delete" width="15" height="15" />';
+			$comments .= '</a>';
+			$comments .= '</td>';
+			$comments .= '</tr>';
 		}
 	}
 
-	if($comments=='')
-		$comments .= "<tr><td colspan='3'>".gettext('There are no comments associated with this host')."</td></tr>";
+	if ($comments == '') {
+		$comments .= '<tr>';
+		$comments .= '<td colspan="3">'.gettext("There are no comments associated with this host").'</td>';
+		$comments .= '</tr>';
+	}
 
 	return $comments;
 }
 
-
-
-
-
 /* expecting values passed from hosts.php or services.php
  * creates page numbers based on how many results are being processed for the tables
  */
-function do_pagenumbers($pageCount,$start,$limit,$resultsCount,$type)
-{
-
+function do_pagenumbers($pageCount, $start, $limit, $resultsCount, $type) {
 	$pagenums = "<div class='pagenumbers'>";
 	$begin = 0;
 	$cp_start = isset($_GET['start']) ? htmlentities($_GET['start']) : 0;
@@ -295,18 +297,23 @@ function do_pagenumbers($pageCount,$start,$limit,$resultsCount,$type)
 	} else {
 		$back_arrow = "<span class='deselect'>$back_arrow_entities</span>";
 	}
-	$pagenums .= "$back_arrow";
+
+	$pagenums .= $back_arrow;
 
 	// Build the direct page links
-	for($i = 0; $i <= $pageCount; $i++)
-	{
+	for ($i = 0; $i <= $pageCount; $i++) {
+
 		//if end is greater than total results, set end to be the resultCount
 		$link = $link_base . "&start=$begin";
 		$page = $i+1;
+
 		//check if the link is the current page
 		//if we're on current page, don't print a link
-		if($cp_start == $begin) $pagenums .= "<span class='deselect'> $page </span>";
-		else $pagenums .= "<a class='pagenumbers' href='$link'> $page </a>";
+		if($cp_start == $begin) {
+			$pagenums .= "<span class='deselect'> $page </span>";
+		} else {
+			$pagenums .= "<a class='pagenumbers' href='$link'> $page </a>";
+		}
 
 		//submit a hidden post page number
 		$begin = ($begin + $limit) < $resultsCount ? ($begin + $limit) : $resultsCount;
@@ -316,133 +323,163 @@ function do_pagenumbers($pageCount,$start,$limit,$resultsCount,$type)
 	$forward_arrow = NULL;
 	$forward_arrow_entities = '&raquo;';
 	if ($cp_start + $limit < $resultsCount) {
-		$link = $link_base . '&start='.($cp_start+$limit);
+		$link = $link_base . '&start='.($cp_start + $limit);
 		$forward_arrow = "<a href='$link' class='pagenumbers'>$forward_arrow_entities</a>";
 	} else {
 		$forward_arrow = "<span class='deselect'>$forward_arrow_entities</span>";
 	}
+
 	$pagenums .= $forward_arrow;
 
-	$pagenums .= "</div><!-- end div.pagenumbers -->\n";
+	$pagenums .= '</div>';
+
 	return $pagenums;
-}	//end do_pagenumbers()
+}
 
 /* creates notes and page limit form above host and service tables
  */
-function do_result_notes($start,$limit,$resultsCount,$type)
-{
+function do_result_notes($start, $limit, $resultsCount, $type) {
 	//check maximum display number for page
-	$end = (($start+$limit)<$resultsCount) ? ($start+$limit) : $resultsCount;
-	$resultnotes = "
-			<div class='tableNotes'>
-				<span class='note' style='vertical-align: bottom;'>Showing results $start - $end of $resultsCount results<br />
-									    ".gettext('Current Result Limit').": $limit</span>
-			</div><!-- end div.tablenotes -->
+	$end = (($start + $limit) < $resultsCount) ? ($start + $limit) : $resultsCount;
 
-		<div class='resultLimit'>
-			<form id='limitform' action='".$_SERVER['PHP_SELF']."?type=$type' method='post'>
-			<label class='label note' for='pagelimit'>".gettext('Limit Results')."</label><br />
-			<select id='pagelimit' name='pagelimit'>";
-			foreach (array(15, 30, 50, 100, 250) as $possible_limit) {
-				$selected = ($possible_limit == $limit) ? "selected='selected'" : NULL;
-				$resultnotes .= "\t\t\t<option value=$possible_limit $selected>$possible_limit</option>\n";
-			}
+	$resultnotes = '
+		<div class="tableNotes">
+			<span class="note" style="vertical-align: bottom;">Showing results '.$start.' - '.$end.' of '.$resultsCount.' results<br />
+				'.gettext('Current Result Limit').': '.$limit.'</span>
+		</div>
+	';
 
-	$resultnotes .= "</select>
-			<input type='submit' name='submitbutton' value='".gettext('Set Limit')."' />
-		</form>
-		</div><!-- end resultLimit --> \n\n";
-	return $resultnotes;
-} //end do_result_notes()
+	$resultnotes .= '
+		<div class="resultLimit">
+			<form id="limitform" action="'.$_SERVER['PHP_SELF'].'?type=$type" method="post">
+			<label class="label note" for="pagelimit">'.gettext("Limit Results").'</label><br />
+			<select id="pagelimit" name="pagelimit">
+	';
 
-
-	//creates state and string filters for host and service tables.  Called on hosts.php and services.php
-	//expecting string :(optional) $name_filter
-	//expecting string: $type = 'host' or 'service'
-function result_filter($name_filter="", $type='host')
-{
-	$ucType = ucfirst($type);
-	$plType = $type.'s';
-	$states = ($type == 'host') ? array('', 'UP', 'DOWN', 'UNREACHABLE', 'PENDING', 'PROBLEMS', 'UNHANDLED', 'ACKNOWLEDGED')
-										: array('', 'OK', 'WARNING', 'CRITICAL', 'UNKNOWN', 'PENDING', 'PROBLEMS', 'UNHANDLED', 'ACKNOWLEDGED');
-
-	///////////////////////////////build filterdiv
-	$resultFilter= "
-
-
-	<form id='resultfilterform' action='".$_SERVER['PHP_SELF']."' method='get'>
-	  <div class='stateFilter'>
-		<input type='hidden' name='type' value=\"".$plType."\">
-		<label class='label note' for='resultfilter'>".gettext('Filter by State').": </label><br />
-		<select id='resultfilter' name='state_filter' onChange='this.form.submit();'>
-";
-
-		foreach ($states as $val)
-		{
-			$selected = (isset($_GET['state_filter']) && $_GET['state_filter'] == $val) ? "selected='selected'" : '';
-			$display_val = $val == '' ? 'None' : $val;
-			$resultFilter.= "\t\t\t<option value=\"$val\" $selected>$display_val</option>\n";
-		}
-
-	$resultFilter.= "
-		</select>
-		</div> <!-- end stateFilter div -->
-
-		<div class='nameFilter'>
-		<label class='label note' for='name_filter'>".gettext('Search')." ".$ucType."name: </label><br />
-		<input type='text' id='name_filter' name='name_filter' value='".$name_filter."'></input>
-		<input type='submit' name='submitbutton' value='Filter' />
-	  </div><!-- end nameFilter div -->
-	</form>
-
-";
-
-	return $resultFilter;
-}//end function result_filter()
-
-//expects a number (percent value)
-//returns an rgb string to use as a css style   "$r,$g,$b"
-function color_code($num)
-{
-    //green is 0,255,0
-    $num=intval($num);
-    $r = 255;
-    $g = 50;
-    $b = 0;
-    //if green >= 255, start subtracting red
-//    $g +=intval($num*2.5);
-    $g +=intval($num*4);
-	if($g>=255)
-	{
-		$r-= ($g-255);
-		$g=255;
+	foreach (array(15, 30, 50, 100, 250) as $possible_limit) {
+		$selected = ($possible_limit == $limit) ? 'selected="selected"' : NULL;
+		$resultnotes .= '<option value="'.$possible_limit.'" '.$selected.'>'.$possible_limit.'</option>';
 	}
 
-    //return into a style : style="background-color: rgb('.$rgb.')
-    $rgb="$r,$g,$b";
-    //print '<div style="background-color: rgb('.$rgb.');">Test</div>';
-    return $rgb;
+	$resultnotes .= '
+			</select>
+			<input type="submit" name="submitbutton" value="'.gettext('Set Limit').'" />
+		</form>
+		</div>';
+
+	return $resultnotes;
 }
 
+/* Creates state and string filters for host and service tables. Called on
+ * hosts.php and services.php
+ *
+ * expecting string :(optional) $name_filter
+ * expecting string: $type = 'host' or 'service'
+ */
+function result_filter($name_filter = '', $type = 'host') {
+	$ucType = ucfirst($type);
+	$plType = $type.'s';
 
-//if status data is being cached, show a link with an option to clear the data and read fresh from status.dat
+	$host_states = array(
+		'',
+		'UP',
+		'DOWN',
+		'UNREACHABLE',
+		'PENDING',
+		'PROBLEMS',
+		'UNHANDLED',
+		'ACKNOWLEDGED'
+	);
+
+	$other_states = array(
+		'',
+		'OK',
+		'WARNING',
+		'CRITICAL',
+		'UNKNOWN',
+		'PENDING',
+		'PROBLEMS',
+		'UNHANDLED',
+		'ACKNOWLEDGED'
+	);
+
+	$states = ($type == 'host') ? $host_states : $other_states;
+
+	//build filterdiv
+	$resultFilter = '
+	<form id="resultfilterform" action="'.$_SERVER["PHP_SELF"].'" method="get">
+	  <div class="stateFilter">
+		<input type="hidden" name="type" value="'.$plType.'">
+		<label class="label note" for="resultfilter">'.gettext("Filter by State").': </label><br />
+		<select id="resultfilter" name="state_filter" onChange="this.form.submit();">
+	';
+
+	foreach ($states as $val) {
+		$selected = (isset($_GET['state_filter']) && $_GET['state_filter'] == $val) ? 'selected="selected"' : '';
+		$display_val = $val == '' ? 'None' : $val;
+		$resultFilter.= '<option value="'.$val.'" '.$selected.'>'.$display_val.'</option>';
+	}
+
+	$resultFilter.= '
+		</select>
+		</div>
+
+		<div class="nameFilter">
+		<label class="label note" for="name_filter">'.gettext("Search").' '.$ucType.'name: </label><br />
+		<input type="text" id="name_filter" name="name_filter" value="'.$name_filter.'"></input>
+		<input type="submit" name="submitbutton" value="Filter" />
+	  </div>
+	</form>
+	';
+
+	return $resultFilter;
+}
+
+/* expects a number (percent value)
+ * returns an rgb string to use as a css style "$r,$g,$b"
+ */
+function color_code($num) {
+
+	//green is 0,255,0
+	$num = intval($num);
+	$r = 255;
+	$g = 50;
+	$b = 0;
+
+	//if green >= 255, start subtracting red
+	//$g += intval($num*2.5);
+	$g += intval($num*4);
+	if ($g >= 255) {
+		$r -= ($g-255);
+		$g = 255;
+	}
+
+	//return into a style : style="background-color: rgb('.$rgb.')
+	$rgb = "$r,$g,$b";
+
+	return $rgb;
+}
+
+/* if status data is being cached, show a link with an option to clear the data 
+ * and read fresh from status.dat
+ */
 function clear_cache_link() {
+	$link ='';
 
-   $link ='';
+	if (function_exists('apc_fetch')) {
 
-   if(function_exists('apc_fetch'))   {
-      $cache = apc_fetch('status_data_exists');
-      if($cache==true) {
-         $query = str_replace('&clearcache=true','',$_SERVER['QUERY_STRING']);
-         $query = ($query == '') ? '?clearcache=true' :  htmlentities('?'.$query.'&clearcache=true');
-         $link = "<div id='clearcache' class='label'>
-                     <a href='".$_SERVER['PHP_SELF'].$query."' title='".gettext('Clear Cached Data')."'>".gettext('Refresh Data')."
-                     </a></div>";
+		$cache = apc_fetch('status_data_exists');
 
-        }
-   }
-   return $link;
+		if ($cache == true) {
+			$query = str_replace('&clearcache=true','',$_SERVER['QUERY_STRING']);
+			$query = ($query == '') ? '?clearcache=true' :  htmlentities('?'.$query.'&clearcache=true');
+			$link = '<div id="clearcache" class="label">
+						<a href="'.$_SERVER["PHP_SELF"].$query.'" title="'.gettext("Clear Cached Data").'">'.gettext("Refresh Data").'</a>
+					</div>';
+		}
+	}
 
+	return $link;
 }
 
 /* End of file data_functions_helper.php */
