190449c5a45d7e321cfa6bea67fc25e843726489
Chris Laskey <contact@chrislaskey.com>
2014-01-09 11:44:56 -0500

    Standardized controller formatting

--- www/application/controllers/services.php
+++ www/application/controllers/services.php
@@ -1,90 +1,91 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Services extends CI_Controller {
+class Services extends CI_Controller
+{
+    // Nagios V-Shell
+    // Copyright (c) 2010 Nagios Enterprises, LLC.
+    // Written by Mike Guthrie <mguthrie@nagios.com>
+    //
+    // LICENSE:
+    //
+    // This work is made available to you under the terms of Version 2 of
+    // the GNU General Public License. A copy of that license should have
+    // been provided with this software, but in any event can be obtained
+    // from http://www.fsf.org.
+    //
+    // This work is distributed in the hope that it will be useful, but
+    // WITHOUT ANY WARRANTY; without even the implied warranty of
+    // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    // General Public License for more details.
+    //
+    // You should have received a copy of the GNU General Public License
+    // along with this program; if not, write to the Free Software
+    // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    // 02110-1301 or visit their web page on the internet at
+    // http://www.fsf.org.
+    //
+    //
+    // CONTRIBUTION POLICY:
+    //
+    // (The following paragraph is not intended to limit the rights granted
+    // to you to modify and distribute this software under the terms of
+    // licenses that may apply to the software.)
+    //
+    // Contributions to this software are subject to your understanding and acceptance of
+    // the terms and conditions of the Nagios Contributor Agreement, which can be found
+    // online at:
+    //
+    // http://www.nagios.com/legal/contributoragreement/
+    //
+    //
+    // DISCLAIMER:
+    //
+    // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+    // INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+    // PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    // HOLDERS BE LIABLE FOR ANY CLAIM FOR DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    // OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+    // GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) OR OTHER
+    // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, STRICT LIABILITY, TORT (INCLUDING
+    // NEGLIGENCE OR OTHERWISE) OR OTHER ACTION, ARISING FROM, OUT OF OR IN CONNECTION
+    // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-	// Nagios V-Shell
-	// Copyright (c) 2010 Nagios Enterprises, LLC.
-	// Written by Mike Guthrie <mguthrie@nagios.com>
-	//
-	// LICENSE:
-	//
-	// This work is made available to you under the terms of Version 2 of
-	// the GNU General Public License. A copy of that license should have
-	// been provided with this software, but in any event can be obtained
-	// from http://www.fsf.org.
-	//
-	// This work is distributed in the hope that it will be useful, but
-	// WITHOUT ANY WARRANTY; without even the implied warranty of
-	// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-	// General Public License for more details.
-	//
-	// You should have received a copy of the GNU General Public License
-	// along with this program; if not, write to the Free Software
-	// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-	// 02110-1301 or visit their web page on the internet at
-	// http://www.fsf.org.
-	//
-	//
-	// CONTRIBUTION POLICY:
-	//
-	// (The following paragraph is not intended to limit the rights granted
-	// to you to modify and distribute this software under the terms of
-	// licenses that may apply to the software.)
-	//
-	// Contributions to this software are subject to your understanding and acceptance of
-	// the terms and conditions of the Nagios Contributor Agreement, which can be found
-	// online at:
-	//
-	// http://www.nagios.com/legal/contributoragreement/
-	//
-	//
-	// DISCLAIMER:
-	//
-	// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-	// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-	// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-	// HOLDERS BE LIABLE FOR ANY CLAIM FOR DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-	// OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-	// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) OR OTHER
-	// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, STRICT LIABILITY, TORT (INCLUDING
-	// NEGLIGENCE OR OTHERWISE) OR OTHER ACTION, ARISING FROM, OUT OF OR IN CONNECTION
-	// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	public function __construct() {
-		parent::__construct();
-	}
+    public function index()
+    {
+        $ci = &get_instance();
+        $services = $ci->nagios_data->grab_details('service');
+        $start = $this->input->get('start');
+        $limit = $this->input->get('limit');
+        $name_filter = $this->input->get('name_filter');
 
-	public function index() {
+        //get variables needed to display page
+        $limit = empty($limit) ? RESULTLIMIT : $limit;
 
-		$ci = &get_instance();
-		$services = $ci->nagios_data->grab_details('service');
-		$start = $this->input->get('start');
-		$limit = $this->input->get('limit');
-		$name_filter = $this->input->get('name_filter');
+        $resultsCount = count($services);
+        //if results are greater than number that the page can display, create page links
+        //calculate number of pages
+        $pageCount = (($resultsCount / $limit) < 1) ? 1 : intval($resultsCount/$limit);
+        $doPagination = $pageCount * $limit < $resultsCount;
 
-		//get variables needed to display page
-		$limit = empty($limit) ? RESULTLIMIT : $limit;
+        $data = array(
+            'services' => $services,
+            'start' => intval($start),
+            'limit' => $limit,
+            'resultsCount' => $resultsCount,
+            'pageCount' => $pageCount,
+            'doPagination' => $doPagination,
+            'name_filter' => $name_filter,
+            );
 
-		$resultsCount = count($services);
-		//if results are greater than number that the page can display, create page links
-		//calculate number of pages
-		$pageCount = (($resultsCount / $limit) < 1) ? 1 : intval($resultsCount/$limit);
-		$doPagination = $pageCount * $limit < $resultsCount;
-
-		$data = array(
-			'services' => $services,
-			'start' => intval($start),
-			'limit' => $limit,
-			'resultsCount' => $resultsCount,
-			'pageCount' => $pageCount,
-			'doPagination' => $doPagination,
-			'name_filter' => $name_filter,
-			);
-
-		$this->load->view('header');
-		$this->load->view('services',$data);
-		$this->load->view('footer');
-	}
+        $this->load->view('header');
+        $this->load->view('services',$data);
+        $this->load->view('footer');
+    }
 }
 
 /* End of file services.php */
