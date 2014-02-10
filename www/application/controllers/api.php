3797ce9a71e56ccfd1f3be65d6a4f8754240249a
mguthrie <mguthrie@redventures.net>
2014-02-09 21:31:47 -0500

    Fix a few bugs for data retrieval in the API

--- www/application/controllers/api.php
+++ www/application/controllers/api.php
@@ -27,10 +27,9 @@ class API extends VS_Controller
      * Fetch status of a certain type
      *
      * @param  string $type 
-     * @param  string $name [description]
-     * @return [type]       [description]
+     * @param  string $host_name
      */
-    public function hoststatus($hostname='') {
+    public function hoststatus($host_name='') {
         
         $Data = $this->nagios_data->get_collection('hoststatus');
 
@@ -44,13 +43,21 @@ class API extends VS_Controller
     }
 
 
-
+    /**
+     * Retrieves a host by numeric ID
+     * @param  int $id host $id property
+     */
     public function host_by_id($id){
         $Data = $this->nagios_data->get_collection('hoststatus');
         $this->output($Data[$id]->to_array());
     }
 
 
+    /**
+     * Retrieve service status objects based on parameters
+     * @param  string $host_name host name filter
+     * @param  string $service   service description (requires host name)
+     */
     public function servicestatus($host_name='',$service=''){
 
         $Data = $this->nagios_data->get_collection('servicestatus');
@@ -58,7 +65,12 @@ class API extends VS_Controller
         //fetch by host name
         if(!empty($host_name)){
 
-            $Data = $Data->get_index_key('host_name',$host_name)->get_where('service_description',$service);
+            if(empty($service)){
+                 $Data = $Data->get_index_key('host_name',$host_name);
+            } else {
+
+                $Data = $Data->get_index_key('host_name',$host_name)->get_where('service_description',$service);
+            }    
 
         }
 
@@ -67,24 +79,39 @@ class API extends VS_Controller
 
 
 
+    /**
+     * Retrieve a single service by $id property
+     * @param  int $id property
+     */
     public function service_by_id($id) {
         $Data = $this->nagios_data->get_collection('servicestatus');
         $this->output($Data[$id]->to_array());
     }
 
 
-
+    /**
+     * Retrieve program status
+     */
     public function programstatus(){
         $Data = $this->nagios_data->get_collection('programstatus');
         $this->output($Data->to_array());
     }
 
+    /**
+     * Retrieve info status
+     * @return [type] [description]
+     */
     public function info(){
         $Data = $this->nagios_data->get_collection('info');
         $this->output($Data->to_array());
     }
 
 
+    /**
+     * Retrieve object data, either the collection or a single record 
+     * @param  string $type Object type
+     * @param  string $name object's name field
+     */
     public function object($type,$name='') {
         $Data = $this->nagios_data->get_collection($type);
 
