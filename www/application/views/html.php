07e10e7b7710cd81257c50e4c95a2cfe33d82225
Chris Laskey <contact@chrislaskey.com>
2014-05-18 15:12:57 -0400

    Added html file and imported header info

---
+++ www/application/views/html.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <!--
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <meta http-equiv="pragma" content="no-cache" />
+    -->
+
+    <title>Nagios V-Shell2</title>
+
+    <link rel="stylesheet" type="text/css" href="<?php echo STATICURL; ?>/css/main.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo STATICURL; ?>/css/footable.core.min.css" />
+
+    <script src="<?php echo STATICURL; ?>/js/jquery-1.11.0.min.js"></script>
+    <script src="<?php echo STATICURL; ?>/js/angular.min.js"></script>
+    <script src="<?php echo STATICURL; ?>/js/jquery.cookie.js"></script>
+    <script src="<?php echo STATICURL; ?>/js/underscore-min.js"></script>
+    <script src="<?php echo STATICURL; ?>/js/moment.min.js"></script>
+    <script src="<?php echo STATICURL; ?>/js/footable.all.min.js"></script>
+    <script src="<?php echo STATICURL; ?>/js/main.js"></script>
+
+</head>
