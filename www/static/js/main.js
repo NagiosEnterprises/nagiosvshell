137fa73600afc6c5a8ded5bd678fe6796004ccf7
Chris Laskey <contact@chrislaskey.com>
2014-07-14 19:44:40 -0400

    Removed deprecated sidebar toggle code

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -30,25 +30,15 @@
     nav.bind = function(){
         var button_open = $('#nav-button-open'),
             button_close = $('#nav-button-close'),
-            sidebar = $('#sidebar'),
             body = $('body');
 
-        button_close.show();
-        button_open.hide();
-
         button_open.on('click', function(){
-            sidebar.show();
-            button_close.show();
-            button_open.hide();
             body
                 .removeClass('sidebar-closed')
                 .addClass('sidebar-open');
         });
 
         button_close.on('click', function(){
-            sidebar.hide();
-            button_close.hide();
-            button_open.show();
             body
                 .removeClass('sidebar-open')
                 .addClass('sidebar-closed');
