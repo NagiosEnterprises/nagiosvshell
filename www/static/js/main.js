b451e4ef2239e39fdd5d6b5b3d1d60df1da1ab9c
Chris Laskey <contact@chrislaskey.com>
2014-07-14 20:35:32 -0400

    Added nav open/close state tracking

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -27,28 +27,65 @@
 
     nav = {};
 
+    nav.open = function(){
+        $('body')
+            .removeClass('sidebar-closed')
+            .addClass('sidebar-open');
+    }
+
+    nav.close = function(){
+        $('body')
+            .removeClass('sidebar-open')
+            .addClass('sidebar-closed');
+    }
+
+    nav.get_cookie = function(){
+        return $.cookie('vshell2_nav');
+    }
+
+    nav.set_cookie = function(value){
+        $.cookie('vshell2_nav', value, { expires: 365, path: '/' });
+    }
+
     nav.bind = function(){
         var button_open = $('#nav-button-open'),
-            button_close = $('#nav-button-close'),
-            body = $('body');
+            button_close = $('#nav-button-close');
 
         button_open.on('click', function(){
-            body
-                .removeClass('sidebar-closed')
-                .addClass('sidebar-open');
+            nav.set_cookie('open');
+            nav.open();
         });
 
         button_close.on('click', function(){
-            body
-                .removeClass('sidebar-open')
-                .addClass('sidebar-closed');
+            nav.set_cookie('close');
+            nav.close();
         });
+    }
+
+    nav.load = function(){
+        var mobile_breakpoint = '480',
+            is_mobile, was_open;
+
+        is_mobile = $(window).width() < mobile_breakpoint ? true : false;
+        was_open = nav.get_cookie() == 'open' ? true : false;
+
+        nav.bind();
+
+        if( is_mobile ){
+            nav.close();
+        }else{
+            if( was_open ){
+                nav.open();
+            }else{
+                nav.close();
+            }
+        }
     }
 
 
 // Document ready
 
     $(document).ready(function(){
-        nav.bind();
+        nav.load();
         tables.pagesize.bind();
     });
